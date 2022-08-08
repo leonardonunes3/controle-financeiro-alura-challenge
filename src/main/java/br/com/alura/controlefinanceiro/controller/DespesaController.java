1d35b84d70b8f624959e62c2479581bb93241aac
leonardonunes3 <leonardo.nunes793@gmail.com>
2022-08-08 00:33:35 -0300

    Realizando a persistencia no banco de dados

--- src/main/java/br/com/alura/controlefinanceiro/controller/DespesaController.java
+++ src/main/java/br/com/alura/controlefinanceiro/controller/DespesaController.java
@@ -1,20 +1,29 @@
 package br.com.alura.controlefinanceiro.controller;
 
 import br.com.alura.controlefinanceiro.model.Despesa;
+import br.com.alura.controlefinanceiro.repository.DespesaRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 @RestController
 @RequestMapping("/despesas")
 public class DespesaController {
 
+    final DespesaRepository despesaRepository;
+
+    public DespesaController(DespesaRepository despesaRepository) {
+        this.despesaRepository = despesaRepository;
+    }
+
     @PostMapping
-    public ResponseEntity<Despesa> teste(@RequestBody Despesa despesa){
+    public ResponseEntity<Despesa> save(@RequestBody @Valid Despesa despesa){
         System.out.println(despesa);
-        return ResponseEntity.ok(despesa);
+        return ResponseEntity.ok(despesaRepository.save(despesa));
     }
 
 }
--- src/main/java/br/com/alura/controlefinanceiro/controller/ReceitaController.java
+++ src/main/java/br/com/alura/controlefinanceiro/controller/ReceitaController.java
@@ -1,16 +1,25 @@
 package br.com.alura.controlefinanceiro.controller;
 
 import br.com.alura.controlefinanceiro.model.Receita;
+import br.com.alura.controlefinanceiro.repository.ReceitaRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/receitas")
 public class ReceitaController {
 
+    final ReceitaRepository receitaRepository;
+
+    public ReceitaController(ReceitaRepository receitaRepository) {
+        this.receitaRepository = receitaRepository;
+    }
+
     @PostMapping
-    public ResponseEntity<Receita> teste(@RequestBody Receita receita){
+    public ResponseEntity<Receita> save(@RequestBody @Valid Receita receita){
         System.out.println(receita);
-        return ResponseEntity.ok(receita);
+        return ResponseEntity.ok(receitaRepository.save(receita));
     }
 }
