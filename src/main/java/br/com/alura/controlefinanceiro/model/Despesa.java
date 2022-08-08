048745b048abd4f6ebc861b9fdf470e52bb65c43
leonardonunes3 <leonardo.nunes793@gmail.com>
2022-08-08 00:31:54 -0300

    Ajuste nas validacoes

--- src/main/java/br/com/alura/controlefinanceiro/model/Despesa.java
+++ src/main/java/br/com/alura/controlefinanceiro/model/Despesa.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -23,9 +24,11 @@ public class Despesa {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotNull
+    @NotNull @NotEmpty
     private String descricao;
+    @NotNull
     private BigDecimal valor;
+    @NotNull
     private LocalDate data;
 
 }
--- src/main/java/br/com/alura/controlefinanceiro/model/Receita.java
+++ src/main/java/br/com/alura/controlefinanceiro/model/Receita.java
@@ -6,6 +6,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
@@ -20,9 +22,12 @@ public class Receita {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Long id;
+    @NotNull @NotEmpty
     private String descricao;
+    @NotNull
     private BigDecimal valor;
+    @NotNull
     private LocalDate data;
 
 }
