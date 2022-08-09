5c7cbc0f9b44bc7352ffcabfe7d9090462acad31
leonardonunes3 <leonardo.nunes793@gmail.com>
2022-08-09 01:33:00 -0300

    Definicao dos metodos da interface

--- src/main/java/br/com/alura/controlefinanceiro/service/DespesaService.java
+++ src/main/java/br/com/alura/controlefinanceiro/service/DespesaService.java
@@ -2,6 +2,12 @@ package br.com.alura.controlefinanceiro.service;
 
 import br.com.alura.controlefinanceiro.model.Despesa;
 
+import java.util.List;
+
 public interface DespesaService {
     Despesa save(Despesa despesa);
+    List<Despesa> findAll();
+    Despesa findById(Long id);
+    Despesa update(Despesa despesa, Long id);
+    void delete(Long id);
 }
--- src/main/java/br/com/alura/controlefinanceiro/service/ReceitaService.java
+++ src/main/java/br/com/alura/controlefinanceiro/service/ReceitaService.java
@@ -2,7 +2,12 @@ package br.com.alura.controlefinanceiro.service;
 
 import br.com.alura.controlefinanceiro.model.Receita;
 
+import java.util.List;
+
 public interface ReceitaService {
     Receita save(Receita receita);
-
+    List<Receita> findAll();
+    Receita findById(Long id);
+    Receita update(Receita receita, Long id);
+    void delete(Long id);
 }
