afb000ba2cc35151d14c760b53cb14fe541c52ca
leonardonunes3 <leonardo.nunes793@gmail.com>
2022-08-09 01:31:27 -0300

    Criacao method query para buscar pela descricao

--- src/main/java/br/com/alura/controlefinanceiro/repository/DespesaRepository.java
+++ src/main/java/br/com/alura/controlefinanceiro/repository/DespesaRepository.java
@@ -4,6 +4,9 @@ import br.com.alura.controlefinanceiro.model.Despesa;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface DespesaRepository extends JpaRepository<Despesa, Long> {
+    List<Despesa> findByDescricao(String descricao);
 }
--- src/main/java/br/com/alura/controlefinanceiro/repository/ReceitaRepository.java
+++ src/main/java/br/com/alura/controlefinanceiro/repository/ReceitaRepository.java
@@ -4,6 +4,10 @@ import br.com.alura.controlefinanceiro.model.Receita;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface ReceitaRepository extends JpaRepository<Receita, Long> {
+    List<Receita> findByDescricao(String descricao);
 }
