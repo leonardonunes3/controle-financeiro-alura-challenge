8edf8d83bfaccc47f228f51f7cf4729f3b1b1b86
leonardonunes3 <leonardo.nunes793@gmail.com>
2022-08-09 01:33:48 -0300

    Implementacao dos metodos aplicando as regras de negocio

--- src/main/java/br/com/alura/controlefinanceiro/service/impl/DespesaServiceImpl.java
+++ src/main/java/br/com/alura/controlefinanceiro/service/impl/DespesaServiceImpl.java
@@ -4,8 +4,9 @@ import br.com.alura.controlefinanceiro.model.Despesa;
 import br.com.alura.controlefinanceiro.repository.DespesaRepository;
 import br.com.alura.controlefinanceiro.service.DespesaService;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
 
 @Service
 public class DespesaServiceImpl implements DespesaService {
@@ -18,6 +19,45 @@ public class DespesaServiceImpl implements DespesaService {
     @Override
     @Transactional
     public Despesa save(Despesa despesa) {
+        List<Despesa> listaDespesaExistente = despesaRepository.findByDescricao(despesa.getDescricao());
+
+        if(listaDespesaExistente != null) {
+            listaDespesaExistente.forEach(r -> {
+                if(r.equals(despesa)) {
+                    throw new RuntimeException("Despesa duplicada");
+                }
+            });
+        }
         return despesaRepository.save(despesa);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Despesa> findAll() {
+        return despesaRepository.findAll();
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Despesa findById(Long id) {
+        return despesaRepository.findById(id).orElseThrow(
+                () -> new RuntimeException("Despesa inexistente"));
+    }
+
+    @Override
+    @Transactional
+    public Despesa update(Despesa despesa, Long id) {
+        despesaRepository.findById(id).orElseThrow(
+                () -> new RuntimeException("Despesa inexistente"));
+
+        despesa.setId(id);
+
+        return save(despesa);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Long id) {
+        despesaRepository.deleteById(id);
     }
 }
--- src/main/java/br/com/alura/controlefinanceiro/service/impl/ReceitaServiceImpl.java
+++ src/main/java/br/com/alura/controlefinanceiro/service/impl/ReceitaServiceImpl.java
@@ -4,8 +4,9 @@ import br.com.alura.controlefinanceiro.model.Receita;
 import br.com.alura.controlefinanceiro.repository.ReceitaRepository;
 import br.com.alura.controlefinanceiro.service.ReceitaService;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
 
 @Service
 public class ReceitaServiceImpl implements ReceitaService {
@@ -18,6 +19,46 @@ public class ReceitaServiceImpl implements ReceitaService {
     @Override
     @Transactional
     public Receita save(Receita receita) {
+        List<Receita> listaReceitaExistente = receitaRepository.findByDescricao(receita.getDescricao());
+
+        if(listaReceitaExistente != null) {
+            listaReceitaExistente.forEach(r -> {
+                if(r.equals(receita)) {
+                    throw new RuntimeException("Receita duplicada");
+                }
+            });
+        }
         return receitaRepository.save(receita);
     }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Receita> findAll() {
+        return receitaRepository.findAll();
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Receita findById(Long id) {
+        return receitaRepository.findById(id).orElseThrow(
+                () -> new RuntimeException("Receita inexistente"));
+    }
+
+    @Override
+    @Transactional
+    public Receita update(Receita receita, Long id) {
+        receitaRepository.findById(id).orElseThrow(
+                () -> new RuntimeException("Receita inexistente"));
+
+        receita.setId(id);
+
+        return save(receita);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Long id) {
+        receitaRepository.deleteById(id);
+    }
+
 }
