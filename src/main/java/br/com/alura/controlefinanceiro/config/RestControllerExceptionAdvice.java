58c47be95d3689fbf11eb9f0ad6b791340671686
leonardonunes3 <leonardo.nunes793@gmail.com>
2022-09-01 18:54:32 -0300

    adicionando classe excecao e handler

--- src/main/java/br/com/alura/controlefinanceiro/config/RestControllerExceptionAdvice.java
+++ src/main/java/br/com/alura/controlefinanceiro/config/RestControllerExceptionAdvice.java
@@ -1,9 +1,11 @@
 package br.com.alura.controlefinanceiro.config;
 
+import br.com.alura.controlefinanceiro.exceptions.ValidacaoException;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -17,23 +19,28 @@ import java.util.stream.Collectors;
 public class RestControllerExceptionAdvice {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+    public ResponseEntity<List<ErrorMessage>> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
         List<ErrorMessage> errorMessageList = ex.getBindingResult()
                 .getAllErrors()
                 .stream()
                 .map(error -> {
-                    return new ErrorMessage( ((FieldError) error).getField(), error.getDefaultMessage() );
+                    return new ErrorMessage(((FieldError) error).getField(), error.getDefaultMessage());
                 })
                 .collect(Collectors.toList());
 
         return ResponseEntity.badRequest().body(errorMessageList);
     }
 
+    @ExceptionHandler(ValidacaoException.class)
+    public ResponseEntity<ErrorMessage> handleValidationException(RuntimeException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorMessage("Validation", ex.getMessage()));
+    }
+
     @Getter
     @Setter
     @NoArgsConstructor
     @AllArgsConstructor
-    class ErrorMessage{
+    static class ErrorMessage{
         private String field;
         private String message;
     }
---
+++ src/main/java/br/com/alura/controlefinanceiro/exceptions/ValidacaoException.java
@@ -0,0 +1,8 @@
+package br.com.alura.controlefinanceiro.exceptions;
+
+public class ValidacaoException extends RuntimeException {
+
+    public ValidacaoException(String message) {
+        super(message);
+    }
+}
